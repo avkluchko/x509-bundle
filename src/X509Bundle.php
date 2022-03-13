9c4149020067d95a9ce44564714df0cdc9c1ab2f
Alexandr Kluchko <kluchko@nagradion.ru>
2022-03-13 08:59:20 +0500

    fixed return type

--- src/X509Bundle.php
+++ src/X509Bundle.php
@@ -3,15 +3,17 @@
 namespace AVKluchko\X509Bundle;
 
 use AVKluchko\X509Bundle\DependencyInjection\X509Extension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class X509Bundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         if (null === $this->extension) {
             $this->extension = new X509Extension();
         }
+
         return $this->extension;
     }
 
